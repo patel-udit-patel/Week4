ea395807f785485a179dfb3da2824f376472d358
patel-udit-patel <udit0428t@gmail.com>
2025-02-13 13:52:41 +0530

    [Udit Patel] Week04 Day03 Problem-> Read User Input from Console submitted on 13-02-25

---
+++ Day03/src/main/java/org/example/read_user_input_from_console/UserInputToFile.java
@@ -0,0 +1,28 @@
+package org.example.read_user_input_from_console;
+
+import java.io.*;
+
+public class UserInputToFile {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileWriter writer = new FileWriter("C:\\Users\\udit0\\OneDrive\\Documents\\CapgeminiTraining\\Assignments\\Week4\\Day03\\src\\main\\java\\org\\example\\read_user_input_from_console\\user_data.txt")) {
+
+            System.out.print("Enter your name: ");
+            String name = reader.readLine();
+
+            System.out.print("Enter your age: ");
+            String age = reader.readLine();
+
+            System.out.print("Enter your favorite programming language: ");
+            String language = reader.readLine();
+
+            writer.write("Name: " + name + "\n");
+            writer.write("Age: " + age + "\n");
+            writer.write("Favorite Programming Language: " + language + "\n");
+
+            System.out.println("User data saved successfully to user_data.txt.");
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
